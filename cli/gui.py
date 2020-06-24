3165c47fe274639dca3ee0dddf1e1f77d92debc4
Blackcats4567 <54811735+Blackcats4567@users.noreply.github.com>
2020-06-25 01:22:05 +0900

    Add files via upload

---
+++ cli/gui.py
@@ -0,0 +1,4 @@
+username = raw_input("Welcome to click HTML for python! Enter your username:")
+print("Welcome back: " + username)
+f = open("click.py", "w")
+print("sorry we are working on it")
---
+++ cli/sandbox.py
@@ -0,0 +1,6 @@
+import datetime
+print("some code experiments will held here! stay tuned :")
+print("==================================================")
+print("Experiment 1")
+x = datetime.datetime.now()
+print(x)
